8c24de3ecdf16a8fe5fbd1dd62fc13f4ffb133e5
Botond Fekete <fekbot@gmail.com>
2024-03-02 15:11:49 +0000

    Add theme switcher

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Screen } from "@/components/screen";
+import { ThemeSwitch } from "@/components/widgets/theme-switch";
 
 export default function Home() {
   return (
     <main className="max-w-screen min-h-screen flex-col items-center justify-between overflow-hidden">
       <Screen />
+      <ThemeSwitch />
     </main>
   );
 }
--- src/components/provider/theme.tsx
+++ src/components/provider/theme.tsx
@@ -1,11 +1,19 @@
 "use client";
 
 import { ReactNode } from "react";
+import { usePathname } from "next/navigation";
 import { ThemeProvider as NextThemeProvider, useTheme } from "next-themes";
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
+  const pathname = usePathname();
+
   return (
-    <NextThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+    <NextThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      forcedTheme={pathname === "/print" ? "light" : undefined}
+    >
       {children}
     </NextThemeProvider>
   );
