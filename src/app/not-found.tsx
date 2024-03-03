da39a4a7a3048b05adab2fee908dd62d47f04754
Botond Fekete <fekbot@gmail.com>
2024-03-03 17:46:40 +0000

    Add 404

---
+++ src/app/not-found.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { ArrowLeftIcon, PawPrint, KeyboardIcon } from "lucide-react";
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+export default function NotFound() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center p-4">
+      <div className="text-center">
+        <div className="mb-12 flex justify-center gap-4">
+          <PawPrint size={64} />
+          <KeyboardIcon size={64} />
+        </div>
+        <h2 className="font-title mb-4 text-4xl">404</h2>
+        <h4 className="font-title text-xl">not found</h4>
+        <p className="my-8">
+          Hi, explorer. Your reward for finding this place is this magic button:
+        </p>
+        <Link
+          href="/"
+          className={cn("gap-2", buttonVariants({ variant: "default" }))}
+        >
+          <ArrowLeftIcon />
+          <span>home</span>
+        </Link>
+      </div>
+    </div>
+  );
+}
